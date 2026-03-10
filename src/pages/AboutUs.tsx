318a50fedd0ccf605aaf943108be13e452fc2ffa
Gevorg_Martirosyan <gevorg.martirosyan.1998.08@gmail.com>
2026-03-10 19:23:48 +0400

    - add header

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,5 +1,13 @@
 export const AboutUs = () => {
     return (
-        <h1>About us page</h1>
+        <>
+            <h1>About us page</h1>
+            <p>
+                Hey, I'm engineer and whole life dream about creating something for me,
+                something interesting, but I have no talents, I'm lazy and boring person,
+                so I decide to create something stupid and boring, maybe it will be interesting for some people,
+                I'm 'motivated' from Rick and Morty series, so maybe II'll copy past a lot things from their world
+            </p>
+        </>
     );
 }
